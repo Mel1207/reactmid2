ad4a529d54dfaecf196fe48db316b5aa10b2d86a
Mel <meljohnpualon@gmail.com>
2021-10-01 00:23:05 +0800

    added change default props to shape

--- src/App.js
+++ src/App.js
@@ -2,28 +2,41 @@ import React, { Component } from 'react'
 import './App.css'
 import PropTypes from 'prop-types'
 
-const Person = ({img, name, age, info}) => {
+const Person = ({ person:{img, name, age, info} }) => {
+  // const {img, name, age, info} = props.person
+
   return(
     <article>
       <img src={img} alt="Image" />
       <h4>name: {name}</h4>
       <h5>age: {age}</h5>
-      <p>info: {info}</p>
+      {/* dirty solution for default */}
+      {/* <p>info: {info || 'default info bout the person'}</p> */}
+      <p>info: {info || 'default info bout the person'}</p>
     </article>
   )
 }
 
 // isRequired and isDefault
+// object with particular shape
 Person.propTypes = {
-  img: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  age: PropTypes.number.isRequired
+  person:PropTypes.shape({
+    img: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number.isRequired
+  })
+  // img: PropTypes.string.isRequired,
+  // name: PropTypes.string.isRequired,
+  // age: PropTypes.number.isRequired
 }
 Person.defaultProps = {
-  img: "https://randomuser.me/api/portraits/thumb/men/45.jpg",
-  name: 'James Doe',
-  age: 30,
-  info: 'this is default info'
+  person: {
+    info: "default info about the person"
+  }
+  // img: "https://randomuser.me/api/portraits/thumb/men/45.jpg",
+  // name: 'James Doe',
+  // age: 30,
+  // info: 'this is default info'
 }
 
 
@@ -38,7 +51,7 @@ class PersonList extends Component {
       },
       {
         id: 2, 
-        // img: "https://randomuser.me/api/portraits/thumb/men/22.jpg",
+        img: "https://randomuser.me/api/portraits/thumb/men/22.jpg",
         name: "John",
         age: 28
       },
@@ -58,10 +71,11 @@ class PersonList extends Component {
         {this.state.people.map((person) => (
           <Person 
             key={person.id} 
-            img={person.img} 
-            name={person.name} 
-            age={person.age} 
-            info={person.info}
+            // img={person.img} 
+            // name={person.name} 
+            // age={person.age} 
+            // info={person.info}
+            person={person}
           />
         ))}
       </section>
