09d5c8ac13d6a8f9f7c478ece29eb6bb0b6ef0e9
Mel <meljohnpualon@gmail.com>
2021-10-02 23:26:55 +0800

    added control inputs

--- src/App.js
+++ src/App.js
@@ -9,21 +9,71 @@ import './App.css'
 // value, onChange
 
 class Form extends Component {
-    render() {
-        return <section>
-            <article>
-                <form>
-                    <input type="text" name="firstName"/>
-                    <input type="text" name="lastName"/>
-                    <button type="submit">Submit</button>
-                </form>
-            </article>
-            <article>
-                <h1>People</h1>
-                <div></div>
+    state = {
+        firstName: '',
+        lastName: '',
+        people: []
+    };
 
-            </article>
-        </section>
+    handleChange = e => {
+        // console.log(e);
+        // console.log(e.target.name);
+        // console.log(e.target.value);
+
+        // const textValue = e.target.value;
+
+        // if(e.target.name === 'firstName') {
+        //     this.setState({
+        //         firstName: textValue,
+        //         // lastName: textValue
+        //     })
+        // }
+
+        this.setState({
+            [e.target.name]:[e.target.value]
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        const firstName = this.state.firstName
+        const lastName = this.state.lastName
+
+        console.log(firstName, lastName)
+
+        if(firstName.length > 0 && lastName.length > 0) {
+            const person = `${firstName} ${lastName} `;
+            this.setState({
+                people:[...this.state.people, person],
+                firstName: "",
+                lastName: ""
+            })
+        } else {
+            alert('please fill all fields')
+        }
+    };
+
+    render() {
+        return (
+            <section>
+                <article>
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange}/>
+                        <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange}/>
+                        <button type="submit">Submit</button>
+                    </form>
+                </article>
+                <article>
+                    <h1>People</h1>
+                    <ul>
+                        <li>{this.state.people}</li>
+                    </ul>
+                    {/* <div>{this.state.people}</div> */}
+
+                </article>
+            </section>
+        )
     }
 }
 
--- src/App2.js
+++ src/App2.js
@@ -1,97 +1,97 @@
-import React, { Component } from 'react'
-import './App.css'
-import PropTypes from 'prop-types'
+// import React, { Component } from 'react'
+// import './App.css'
+// import PropTypes from 'prop-types'
 
-const Person = ({ person:{img, name, age, info} }) => {
-  // const {img, name, age, info} = props.person
+// const Person = ({ person:{img, name, age, info} }) => {
+//   // const {img, name, age, info} = props.person
 
-  return(
-    <article>
-      <img src={img} alt="Image" />
-      <h4>name: {name}</h4>
-      <h5>age: {age}</h5>
-      {/* dirty solution for default */}
-      {/* <p>info: {info || 'default info bout the person'}</p> */}
-      <p>info: {info || 'default info bout the person'}</p>
-    </article>
-  )
-}
+//   return(
+//     <article>
+//       <img src={img} alt="Image" />
+//       <h4>name: {name}</h4>
+//       <h5>age: {age}</h5>
+//       {/* dirty solution for default */}
+//       {/* <p>info: {info || 'default info bout the person'}</p> */}
+//       <p>info: {info || 'default info bout the person'}</p>
+//     </article>
+//   )
+// }
 
-// isRequired and isDefault
-// object with particular shape
-Person.propTypes = {
-  person:PropTypes.shape({
-    img: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    age: PropTypes.number.isRequired
-  })
-  // img: PropTypes.string.isRequired,
-  // name: PropTypes.string.isRequired,
-  // age: PropTypes.number.isRequired
-}
-Person.defaultProps = {
-  person: {
-    info: "default info about the person"
-  }
-  // img: "https://randomuser.me/api/portraits/thumb/men/45.jpg",
-  // name: 'James Doe',
-  // age: 30,
-  // info: 'this is default info'
-}
-
-
-class PersonList extends Component {
-  state = {
-    people: [
-      {
-        id: 1, 
-        img: "https://randomuser.me/api/portraits/thumb/men/75.jpg",
-        name: "Mel",
-        // age: 26
-      },
-      {
-        id: 2, 
-        img: "https://randomuser.me/api/portraits/thumb/men/22.jpg",
-        name: "John",
-        age: 28
-      },
-      {
-        id: 3, 
-        img: "https://randomuser.me/api/portraits/thumb/men/23.jpg",
-        name: "Doe",
-        age: 28,
-        info: "some info about me"
-      }
-    ]
-  }
-
-  render() {
-    return (
-      <section>
-        {this.state.people.map((person) => (
-          <Person 
-            key={person.id} 
-            // img={person.img} 
-            // name={person.name} 
-            // age={person.age} 
-            // info={person.info}
-            person={person}
-          />
-        ))}
-      </section>
-    )
-  }
-}
+// // isRequired and isDefault
+// // object with particular shape
+// Person.propTypes = {
+//   person:PropTypes.shape({
+//     img: PropTypes.string.isRequired,
+//     name: PropTypes.string.isRequired,
+//     age: PropTypes.number.isRequired
+//   })
+//   // img: PropTypes.string.isRequired,
+//   // name: PropTypes.string.isRequired,
+//   // age: PropTypes.number.isRequired
+// }
+// Person.defaultProps = {
+//   person: {
+//     info: "default info about the person"
+//   }
+//   // img: "https://randomuser.me/api/portraits/thumb/men/45.jpg",
+//   // name: 'James Doe',
+//   // age: 30,
+//   // info: 'this is default info'
+// }
 
 
-export default class App2 extends Component {
-  render() {
-    return (
-      <div>
-        <h2>hey hey hey hey</h2>
-        <p>this is nesw</p>
-        <PersonList />
-      </div>
-    )
-  }
-}
+// class PersonList extends Component {
+//   state = {
+//     people: [
+//       {
+//         id: 1, 
+//         img: "https://randomuser.me/api/portraits/thumb/men/75.jpg",
+//         name: "Mel",
+//         // age: 26
+//       },
+//       {
+//         id: 2, 
+//         img: "https://randomuser.me/api/portraits/thumb/men/22.jpg",
+//         name: "John",
+//         age: 28
+//       },
+//       {
+//         id: 3, 
+//         img: "https://randomuser.me/api/portraits/thumb/men/23.jpg",
+//         name: "Doe",
+//         age: 28,
+//         info: "some info about me"
+//       }
+//     ]
+//   }
+
+//   render() {
+//     return (
+//       <section>
+//         {this.state.people.map((person) => (
+//           <Person 
+//             key={person.id} 
+//             // img={person.img} 
+//             // name={person.name} 
+//             // age={person.age} 
+//             // info={person.info}
+//             person={person}
+//           />
+//         ))}
+//       </section>
+//     )
+//   }
+// }
+
+
+// export default class App2 extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h2>hey hey hey hey</h2>
+//         <p>this is nesw</p>
+//         <PersonList />
+//       </div>
+//     )
+//   }
+// }
